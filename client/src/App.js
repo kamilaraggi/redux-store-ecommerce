5f70813e82084201c903622ba489d3dc4858ae13
kamilaraggi <kamilaraggi@hotmail.com>
2022-01-05 20:47:39 -0500

    add useStoreContext

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { StoreProvider } from "./utils/GlobalState";
 
 import Home from './pages/Home';
 import Detail from './pages/Detail';
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,7 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
+
+import Cart from '../components/Cart';
+// useStoreContext 
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+ } from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
+import { idbPromise } from '../utils/helpers';
+
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
